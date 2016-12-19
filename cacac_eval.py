886966c6af230fc0513ba47b728e3afc1e848941
tzirakis <ptzirakis88@yahoo.com>
2016-12-19 16:39:24 +0000

    Update cacac_eval.py

--- cacac_eval.py
+++ cacac_eval.py
@@ -19,11 +19,12 @@ tf.app.flags.DEFINE_string('pretrained_model_checkpoint_path', '',
                           '''before beginning any training.''')
 tf.app.flags.DEFINE_integer('batch_size', 15, '''The batch size to use.''')
 tf.app.flags.DEFINE_string('model', 'audio','''Which model is going to be used: audio,video, or both ''')
-tf.app.flags.DEFINE_string('dataset_dir', './tf_records/', 'The tfrecords directory.')
+tf.app.flags.DEFINE_string('dataset_dir', 'CACAC/tf_records', 'The tfrecords directory.')
 tf.app.flags.DEFINE_string('checkpoint_dir', './ckpt/train/', 'The tfrecords directory.')
 tf.app.flags.DEFINE_string('log_dir', './ckpt/logs/valid/', 'The tfrecords directory.')
-tf.app.flags.DEFINE_string('num_examples', 10000, 'The number of examples in the test set')
+tf.app.flags.DEFINE_string('num_examples', 3550, 'The number of examples in the test set')
 tf.app.flags.DEFINE_string('eval_interval_secs', 300, 'The number of examples in the test set')
+tf.app.flags.DEFINE_string('mode', 'devel', 'The number of examples in the test set')
 
 def evaluate(data_folder):
 
@@ -31,37 +32,34 @@ def evaluate(data_folder):
   with g.as_default():
     
     # Load dataset.
-    audio, ground_truth = data_provider.get_split(data_folder, 'train', FLAGS.batch_size)
+    audio, labels = data_provider.get_split(data_folder, FLAGS.mode, FLAGS.batch_size)
     
     # Define model graph.
     with slim.arg_scope([slim.batch_norm, slim.layers.dropout],
                            is_training=False):
-      prediction = models.get_model(FLAGS.model)(audio)
+      predictions = models.get_model(FLAGS.model)(audio)
 
-      '''
-      num_examples = FLAGS.num_examples
-      num_batches = num_examples / (FLAGS.batch_size)
-      logging.set_verbosity(1)
-      
-      
-      # Evaluate model
-      correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(ground_truth, 1))
-      
-      for i in range(num_batches):
-        acc = tf.add(acc, sess.run(tf.cast(correct_pred, tf.float32)))
+      names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+          "eval/accuracy": slim.metrics.streaming_accuracy(predictions, labels),
+         # "eval/TP": slim.metrics.streaming_true_positives(predictions, labels),
+         # "eval/FN": slim.metrics.streaming_false_negatives(predictions, labels),
+         # "eval/TN": slim.metrics.streaming_true_negatives(predictions, labels),
+         # "eval/FP": slim.metrics.streaming_false_positives(predictions, labels),
+      })
 
-      accuracy = tf.reduce_mean(acc)        
-      '''
+      # Create the summary ops such that they also print out to std output:
+      # Create the summary ops such that they also print out to std output:
+      #recall_1 = names_to_values['eval/TP'] / (names_to_values['eval/TP'] + names_to_values['eval/FN'])
+      #recall_2 = names_to_values['eval/TN'] / (names_to_values['eval/TN'] + names_to_values['eval/FP'])
+      #op2 = tf.summary.scalar('UAR', tf.truediv(tf.add(recall_1, recall_2), 2))
 
-
-      names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({"eval/precision": slim.metrics.streaming_precision(prediction, ground_truth)})
-      
       # Create the summary ops such that they also print out to std output:
       summary_ops = []
 
       op = tf.summary.scalar(names_to_values.keys()[0], names_to_updates.values()[0])
       op = tf.Print(op, [names_to_updates.values()[0]], names_to_values.keys()[0])
       summary_ops.append(op)
+      #summary_ops.append(op2)
 
       num_examples = FLAGS.num_examples
       num_batches = num_examples / (FLAGS.batch_size)
