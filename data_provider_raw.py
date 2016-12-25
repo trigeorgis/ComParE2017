a6e40b7e4df862dd92220cac1109e8d5f339c230
tzirakis <ptzirakis88@yahoo.com>
2016-12-25 08:13:42 +0000

    Update data_provider_raw.py

--- data_provider_raw.py
+++ data_provider_raw.py
@@ -18,7 +18,7 @@ def read_wav_file(wav_file):
         The audio data.
     """
 
-    fp = wave.open(wav_file)
+    fp = wave.open(str(wav_file))
     nchan = fp.getnchannels()
     N = fp.getnframes()
     dstr = fp.readframes(N*nchan)
@@ -49,11 +49,10 @@ def get_split(dataset_dir, split_name='train'):
     files = list(reader)
 
   files = [(x) for x in files if split_name in x[1]]
-  audio_files = [dataset_dir + split_name +'/'+ str(x[0]) for x in files]
+  audio_files = [Path(dataset_dir) / split_name / str(x[0]) for x in files]
   labels = [str(x[2]) =='CDS' for x in files if str(x[1]) in split_name]
   labels = encoder(labels)
 
-  is_training = split_name == 'train'
   data = []
   for i in range(len(audio_files)):
     wav = read_wav_file(audio_files[i])
