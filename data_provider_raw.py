6ded4fd77e01d6447a188c4bf491362f7f71ca97
tzirakis <ptzirakis88@yahoo.com>
2016-12-25 06:36:32 +0000

    Update data_provider_raw.py

--- data_provider_raw.py
+++ data_provider_raw.py
@@ -1,60 +1,65 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
+import numpy as np
+import wave
+import struct
 import csv
-import tensorflow as tf
 from pathlib import Path
-from scipy.io import wavfile
-from tensorflow.contrib import ffmpeg
-
-slim = tf.contrib.slim
 
 
-def read_files(filename_queue):
-
-  audio_binary = tf.read_file(filename_queue[0])
-  raw_audio = ffmpeg.decode_audio(audio_binary, file_format='wav', samples_per_second=16000, channel_count=1)
-  
-  return raw_audio, filename_queue[1] == 'CDS'
-
-def get_split(dataset_dir, split_name='train', batch_size=32):
-    """Returns a data split of the RECOLA dataset.
+def read_wav_file(wav_file):
+    """Read a wav file.
     
     Args:
-        split_name: A train/test/valid split name.
+        wav_file: The name of the wav file
     Returns:
-        The raw audio examples and the corresponding arousal/valence
-        labels.
+        The audio data.
     """
-    with open('CACAC/labels.txt') as f:
-      reader = csv.reader(f, delimiter=";")
-      files = list(reader)
 
-    files = [(x) for x in files if split_name in x[1]]
-    paths = [dataset_dir + str(x[0])+','+str(x[2]) for x in files]
-    audio_files = [dataset_dir + str(x[0]) for x in files]
-    labels = [str(x[2]) for x in files]
+    fp = wave.open(wav_file)
+    nchan = fp.getnchannels()
+    N = fp.getnframes()
+    dstr = fp.readframes(N*nchan)
+    data = np.fromstring(dstr, np.int16)
+    audio = np.reshape(data, (-1))
 
-    is_training = split_name == 'train'
-    filename_queue = tf.train.slice_input_producer([audio_files, labels],
-                                            shuffle=is_training)
-    #filename_queue = tf.train.string_input_producer(paths, shuffle=is_training)
+    audio = np.pad(audio, (0, 640 - audio.shape[0] % 640), 'constant')
+    audio = audio.reshape(-1, 640)
 
-    raw_audio, label = read_files(filename_queue)
+    return audio
+
+def encoder(labels):
+
+  data = []
+  for i in range(len(labels)):
+    if labels[i] == True:
+      data.append(np.array([1,0]))
+    else:
+      data.append(np.array([0,1]))
+  data = np.vstack(data)
   
-    raw_audio.set_shape([640,1])
-    label = tf.cast(label,tf.uint8)
-    #label.set_shape([1])    
+  return data
 
-    raw_audio, label = tf.train.shuffle_batch(
-        [raw_audio, label], 1, 5000, 500, 4)
+def get_split(dataset_dir, split_name='train'):
 
-    #raw_audio = tf.decode_raw(raw_audio, tf.float32)
+  with open('CACAC/labels.txt') as f:
+    reader = csv.reader(f, delimiter=";")
+    files = list(reader)
 
-    frames, labels = tf.train.batch([raw_audio, label[0]], batch_size,
-                                    capacity=1000, dynamic_pad=True)
+  path = Path(dataset_dir) / split_name 
+  audio_files = [str(x) for x in path.glob('*.wav')]
 
-    frames = tf.reshape(frames, (batch_size, -1, 640))
-    labels = slim.one_hot_encoding(labels, 2)
-    
-    return frames, labels
+  labels = [str(x[2]) =='CDS' for x in files if str(x[1]) in split_name]
+  labels = encoder(labels)
+
+  is_training = split_name == 'train'
+  data = []
+  for i in range(len(audio_files)):
+    wav = read_wav_file(audio_files[i])
+    data.append(wav)
+  
+  data = np.vstack(data)
+  
+  return data, labels
