93992c1356b073e7ff40de95caf16271ac29f14f
tzirakis <ptzirakis88@yahoo.com>
2016-12-29 16:17:28 +0000

    Update data_provider.py

--- data_provider.py
+++ data_provider.py
@@ -3,8 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-from pathlib import Path
 
+from pathlib import Path
 
 slim = tf.contrib.slim
 
@@ -15,56 +15,54 @@ _split_to_num_samples = {
 }
 
 def get_split(dataset_dir, split_name='train', batch_size=32):
-    """Returns a data split of the RECOLA dataset.
-    
-    Args:
-        dataset_dir: The directory that contains the data.
-        split_name: A train/test/valid split name.
-        batch_size: The size of the batch.
-    Returns:
-        The raw audio examples and the corresponding arousal/valence
-        labels.
-    """
-    
-    paths = [str(Path(dataset_dir) / '{}.tfrecords'.format(name)) 
-             for name in split_name.split(',')]
+  """Returns a data split of the RECOLA dataset.
+  
+  Args:
+      dataset_dir: The directory that contains the data.
+      split_name: A train/test/valid split name.
+      batch_size: The size of the batch.
+  Returns:
+      The raw audio examples and the corresponding arousal/valence
+      labels.
+  """
+  
+  paths = [str(Path(dataset_dir) / '{}.tfrecords'.format(name)) 
+           for name in split_name.split(',')]
 
-    is_training = 'train' in split_name
+  is_training = 'train' in split_name
 
-    filename_queue = tf.train.string_input_producer(paths, shuffle=is_training)
+  filename_queue = tf.train.string_input_producer(paths, shuffle=is_training)
 
-    reader = tf.TFRecordReader()
+  reader = tf.TFRecordReader()
 
-    _, serialized_example = reader.read(filename_queue)
+  _, serialized_example = reader.read(filename_queue)
 
-    features = tf.parse_single_example(
-            serialized_example,
-            features={
-                'raw_audio': tf.FixedLenFeature([], tf.string),
-                'label': tf.FixedLenFeature([], tf.int64),
-                }
-            )
+  features = tf.parse_single_example(
+          serialized_example,
+          features={
+              'raw_audio': tf.FixedLenFeature([], tf.string),
+              'label': tf.FixedLenFeature([], tf.int64),
+              }
+          )
 
-    raw_audio = features['raw_audio']
-    label = features['label']
+  raw_audio = features['raw_audio']
+  label = features['label']
 
-    if is_training:
-        raw_audio, label = tf.train.shuffle_batch(
-                [raw_audio, label], 1, 5000, 500, 4)
-        raw_audio = raw_audio[0]
-        label = label[0]
+  if is_training:
+    raw_audio, label = tf.train.shuffle_batch(
+            [raw_audio, label], 1, 5000, 500, 4)
+    raw_audio = raw_audio[0]
+    label = label[0]
 
-    raw_audio = tf.decode_raw(raw_audio, tf.float32)
+  raw_audio = tf.decode_raw(raw_audio, tf.float32)
 
-    if is_training:
-        raw_audio += tf.random_normal(tf.shape(raw_audio), stddev=.25)
+  if is_training:
+    raw_audio += tf.random_normal(tf.shape(raw_audio), stddev=.25)
 
-    frames, labels = tf.train.batch([raw_audio, label], batch_size,
-                                  capacity=1000, dynamic_pad=True)
+  frames, labels = tf.train.batch([raw_audio, label], batch_size,
+                                capacity=1000, dynamic_pad=True)
 
-
-
-    frames = tf.reshape(frames, (batch_size, -1, 640))
-    labels = slim.one_hot_encoding(labels, 2)
-    
-    return frames, labels, sum(_split_to_num_samples[name] for name in split_name.split(','))
+  frames = tf.reshape(frames, (batch_size, -1, 640))
+  labels = slim.one_hot_encoding(labels, 2)
+  
+  return frames, labels, sum(_split_to_num_samples[name] for name in split_name.split(','))
