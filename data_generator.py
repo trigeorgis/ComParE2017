37616706246b82721296f13c349641b14673c193
tzirakis <ptzirakis88@yahoo.com>
2016-12-29 11:15:20 +0000

    Create data_generator.py

---
+++ data_generator.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import numpy as np
+import argparse
+
+from moviepy.editor import AudioFileClip
+from pathlib import Path
+
+def get_labels(root_dir):
+  root_dir = Path(root_dir)
+  with open((root_dir / 'labels.txt').as_posix()) as f:
+    raw_labels = [x.strip().split(';') for x in f.readlines()]
+    
+    labels = {}
+    for name, portion, g in raw_labels:
+      labels.setdefault(portion, []).append((name, g == 'CDS'))
+
+  return labels
+
+def get_audio(wav_file, root_dir):
+  file = AudioFileClip(str(root_dir /  wav_file))
+  file = file.set_fps(16000)
+  audio = np.array(list(file.iter_frames()))[:, 0]
+  audio = np.pad(audio, (0, 640 - audio.shape[0] % 640), 'constant')
+  audio = audio.reshape(-1, 640)
+
+  return audio
+
+def _int_feauture(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feauture(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def serialize_sample(writer, sample_data, root_dir):
+  for i, (wav_file, label) in enumerate(sample_data):
+
+    audio = get_audio(wav_file, root_dir)
+    example = tf.train.Example(features=tf.train.Features(feature={
+                'label': _int_feauture(label),
+                'raw_audio': _bytes_feauture(audio.tobytes()),
+            }))
+
+    writer.write(example.SerializeToString())
+    del audio, label
+
+def main(data_folder, labels_file, tfrecords_folder):
+
+  root_dir = Path(data_folder)
+  labels = get_labels(labels_file)
+  for portion in labels:
+    print('Createing {} tfrecord file'.format(portion))
+    writer = tf.python_io.TFRecordWriter(
+        (Path(tfrecords_folder) / '{}.tfrecords'.format(portion)
+    ).as_posix())
+    
+    serialize_sample(writer, labels[portion], root_dir)
+    writer.close()
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", "--dbfolder", help="The folder that contains the wav fiels", type=str, default=".")
+  parser.add_argument("-l", "--lbfolder", help="The folder that contains the labels.txt file", type=str, default=".")
+  parser.add_argument("-w", "--tffolder", help="The folder to write the tf records", type=str, default=".")
+  args = parser.parse_args()
+
+  main(args.dbfolder, args.lbfolder, args.tffolder)
+
+  
