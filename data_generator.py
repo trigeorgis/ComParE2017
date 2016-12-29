736a105ffe360170dbf1d2f6b1f66281479d24d7
tzirakis <ptzirakis88@yahoo.com>
2016-12-29 17:05:04 +0000

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,31 +1,42 @@
 import tensorflow as tf
 import numpy as np
-import argparse
+import wave
 
-from moviepy.editor import AudioFileClip
 from pathlib import Path
 
-# Create FLAGS
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('db_folder', 'CACAC/all/', 'The folder that contains the wav files.')
-tf.app.flags.DEFINE_string('labels_folder', 'CACAC/', 'The folder that contains the labels.txt file.')
+tf.app.flags.DEFINE_string('wave_folder', 'CACAC/all/', 'The folder that contains the wav files.')
+tf.app.flags.DEFINE_string('labels_folder', 'CACAC/labels.txt', 'The folder that contains the labels.txt file.')
 tf.app.flags.DEFINE_string('tf_folder', 'CACAC/all/tf_records', 'The folder to write the tf records.')
 
 def get_labels(root_dir):
+  
   root_dir = Path(root_dir)
-  with open((root_dir / 'labels.txt').as_posix()) as f:
+  with open((root_dir).as_posix()) as f:
     raw_labels = [x.strip().split(';') for x in f.readlines()]
-    
-    labels = {}
-    for name, portion, g in raw_labels:
-      labels.setdefault(portion, []).append((name, g == 'CDS'))
+  
+  labels = {}
+  for name, portion, g in raw_labels:
+    labels.setdefault(portion, []).append((name, g == 'CDS'))
 
   return labels
 
 def get_audio(wav_file, root_dir):
-  file = AudioFileClip(str(root_dir /  wav_file))
-  file = file.set_fps(16000)
-  audio = np.array(list(file.iter_frames()))[:, 0]
+  """Read a wav file.
+  
+  Args:
+      wav_file: The name of the wav file
+  Returns:
+      The audio data.
+  """
+
+  fp = wave.open(str(root_dir / wav_file))
+  nchan = fp.getnchannels()
+  N = fp.getnframes()
+  dstr = fp.readframes(N*nchan)
+  data = np.fromstring(dstr, np.int16)
+  audio = np.reshape(data, (-1))
+
   audio = np.pad(audio, (0, 640 - audio.shape[0] % 640), 'constant')
   audio = audio.reshape(-1, 640)
 
@@ -38,6 +49,7 @@ def _bytes_feauture(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def serialize_sample(writer, sample_data, root_dir):
+  
   for i, (wav_file, label) in enumerate(sample_data):
 
     audio = get_audio(wav_file, root_dir)
@@ -63,4 +75,4 @@ def main(data_folder, labels_file, tfrecords_folder):
     writer.close()
 
 if __name__ == '__main__':
-  main(FLAGS.db_folder, FLAGS.labels_folder, FLAGS.tf_folder)
+  main(FLAGS.wave_folder, FLAGS.labels_folder, FLAGS.tf_folder)
