b10c2e8fa19f2f92a7b5978c2b3e00e7e221da27
Panagiotis Tzirakis <pt511@graphic20.doc.ic.ac.uk>
2017-02-21 16:25:42 +0000

    refactore run_raw

--- run_raw.py
+++ run_raw.py
@@ -13,20 +13,16 @@ from menpo.visualize import print_progress
 
 slim = tf.contrib.slim
 
+dataset_dir = Path('/vol/atlas/homes/gt108/db/SNORE/dist/wav/')
 
-def evaluate_raw(input):
-
-#  wav_file = Path('test_0107.wav')
-#  input = dg.get_audio(wav_file, dataset_dir)
-#  input = np.expand_dims(input, axis=0)
-
+def evaluate_raw(file_list):
 
   g = tf.Graph()
   with g.as_default():
 
     with slim.arg_scope([slim.batch_norm],
                            is_training=False):
-       audio = tf.placeholder('float', [1, input.shape[1], 640])
+       audio = tf.placeholder(tf.float32, [1, None, 640])
        predictions = models.get_model('audio')(audio)
 
     coord = tf.train.Coordinator()
@@ -36,45 +32,43 @@ def evaluate_raw(input):
     model_path = 'best_models/LSTM2/model.ckpt-28223'
 #    model_path = 'best_models/LSTM3/model.ckpt-12024'
 
+    labels_idx = {'0':'V', '1':'E', '2':'O', '3':'T'}
     with tf.Session() as sess:
-        saver.restore(sess, model_path)  
+        saver.restore(sess, model_path)
         tf.train.start_queue_runners(sess=sess)
 
         try:
+          predicts = []
+          for i in print_progress(range(len(file_list))):
+            wav_file = Path(file_list[i])
+            input = dg.get_audio(wav_file, dataset_dir)
+            input = np.expand_dims(input, axis=0)
+
             pr = sess.run([predictions], feed_dict={audio:input})
             a = np.argmax(pr)
 
+            predicts.append(labels_idx[str(a)])
+
             coord.request_stop()
         except Exception as e:
-            print('WHAT : ', e)
             coord.request_stop(e)
 
-    return a
+    return predicts
 
 def main():
-  dataset_dir = Path('/vol/atlas/homes/gt108/db/SNORE/dist/wav/')
   file_list = [x for x in os.listdir(str(dataset_dir)) if 'devel' in x]
 
+#  labels_idx = {'V': 0, 'E': 1, 'O': 2, 'T': 3}
+#  labels_idx = {'0':'V', '1':'E', '2':'O', '3':'T'}
+  pr = evaluate_raw(file_list)
+  print(pr)
 
-  labels_idx = {'0':'V', '1':'E', '2':'O', '3':'T'}
-  predictions = []
-  files = []
-  for i in range(len(file_list)):
-    wav_file = Path(file_list[i])
-    input = dg.get_audio(wav_file, dataset_dir)
-    input = np.expand_dims(input, axis=0)
-    print(input.shape)
-    return
-    pr = evaluate_raw(input)
-    predictions.append(labels_idx[str(pr)])
-    files.append(file_list[i])
-
-  files = np.array(files).reshape((-1,1))
-  predictions = np.array(predictions).reshape((-1,1))
+  files = np.array(file_list).reshape((-1,1))
+  predictions = np.array(pr).reshape((-1,1))
 
   data = np.hstack([files, predictions])
   print(data)
-  np.savetxt('LSTM2_results_devel.txt', data,  fmt='%.18s')
+#  np.savetxt('LSTM2_results_devel.txt', data,  fmt='%.18s')
 
 if __name__ == '__main__':
   main()
